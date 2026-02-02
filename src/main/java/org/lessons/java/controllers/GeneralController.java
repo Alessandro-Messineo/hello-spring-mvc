6847c1bc1e3f19aa15f4f96987ff51c054a3ba56
AlessandroMessineo <alessandromessineo46@gmail.com>
2026-02-02 16:45:31 +0100

    creata rotta profile

--- src/main/java/org/lessons/java/controllers/GeneralController.java
+++ src/main/java/org/lessons/java/controllers/GeneralController.java
@@ -1,8 +1,10 @@
 package org.lessons.java.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -11,5 +13,12 @@ public class GeneralController {
     @GetMapping("/")
     public String home(){
         return "home";
+    }
+
+    @GetMapping("/profile")
+    public String profile( Model model, @RequestParam(name = "name") String name){
+        model.addAttribute("name", name);
+        model.addAttribute("post", (int)(Math.random() * 100));
+        return "profile";
     }
 }
