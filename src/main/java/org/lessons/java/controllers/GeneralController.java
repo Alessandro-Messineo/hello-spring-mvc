66e3fd915962509794614364d3adf6ff73d5d585
AlessandroMessineo <alessandromessineo46@gmail.com>
2026-02-02 17:55:54 +0100

    creata rotta stats

--- src/main/java/org/lessons/java/controllers/GeneralController.java
+++ src/main/java/org/lessons/java/controllers/GeneralController.java
@@ -9,16 +9,33 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/")
 public class GeneralController {
-    
+
     @GetMapping("/")
-    public String home(){
+    public String home() {
         return "home";
     }
 
     @GetMapping("/profile")
-    public String profile( Model model, @RequestParam(name = "name") String name){
+    public String profile(Model model, @RequestParam(name = "name") String name) {
         model.addAttribute("name", name);
-        model.addAttribute("post", (int)(Math.random() * 100));
+        model.addAttribute("post", (int) (Math.random() * 100));
         return "profile";
+    }
+
+    @GetMapping("/stats")
+    public String stats(Model model) {
+        int visite = (int) (Math.random() * 100);
+        String messaggio;
+
+        if (visite > 50) {
+            messaggio = "Il sito è popolare";
+        } else {
+            messaggio = "Il sito non è popolare";
+        }
+
+        model.addAttribute("visite", visite);
+        model.addAttribute("messaggio", messaggio);
+
+        return "stats";
     }
 }
